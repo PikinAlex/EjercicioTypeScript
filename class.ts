79cb183f5afb50b198a57404e66135044dc7198c
PikinAlex <alexis_usiel@hotmail.com>
2019-08-29 00:23:12 -0600

    Parte 5 Genericos

--- class.ts
+++ class.ts
@@ -1,29 +1,7 @@
-class Person {
-    private name: string;
-    protected age: number;
-    protected constructor(theName: string, theAge: number) {
-    this.name = theName;
-    this.age = theAge;
+function randomIntElem(theArray: number[]): number {
+    let randomIndex = Math.floor(Math.random()*theArray.length);
+    return theArray[randomIndex];
     }
-    introduceSelf() {
-    console.log("Hi, I am " + this.name + "!");
-    }
-    }
-    class Friend extends Person {
-    yearsKnown: number;
-    constructor(name: string, age: number, yearsKnown: number) {
-    super(name, age);
-    this.yearsKnown = yearsKnown;
-    }
-    timeKnown() {
-    console.log("We have been friends for " + this.yearsKnown + " years.")
-    }
-    friendSince() {
-    let firstAge = this.age - this.yearsKnown;
-    console.log(`We have been friends since I was ${firstAge} years old.`)
-    }
-    }
-    let friendA = new Friend("William", 19, 8);
-    // Prints: We have been friends since I was 11 years old.
-    friendA.friendSince();
+    let positions: number[] = [103, 458, 472, 458];
+    let randomPosition: number = randomIntElem(positions);
     
